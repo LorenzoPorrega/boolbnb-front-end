6dafb5a249ece2ab380c88ac6ff6beda9ffa71cf
acolombo09 <acolombo0911@gmail.com>
2023-11-06 15:18:26 +0100

    cta-section-added

--- src/pages/ApartmentsPage.vue
+++ src/pages/ApartmentsPage.vue
@@ -24,13 +24,52 @@ export default {
 </script>
 
 <template>
-  <div class="container text-center border border-danger" style="height: 300px;">
+  <div class="container text-center border border-danger">
     <h1>Apartments</h1>
+
+    <!-- CTA section -->
+    <div class="container-fluid p-3 justify-content-center border border-info">
+      <div class="row my-3 justify-content-center">
+        <div class="col text-center">
+          <div class="card border-0">
+            <div class="card-body">
+                <div class="card-title">
+                  <h4 class="mb-3">News & Discounts</h4>
+                </div> 
+                <div class="row justify-content-center">
+                  <div class="col-md-4">
+                    <p class="small color-text">
+                      Find your coziest escape ever & Get instant discounts. 
+                      Discover cabins, vacation homes, and more!
+                    </p>
+                    <button type="button" class="btn btn-primary border-0 my-3">
+                      Get in Touch
+                    </button><br>
+                    <img src="https://i.imgur.com/pC6AgYC.jpg" class="img-fluid" width="300">
+                  </div>
+                </div>
+            </div>                       
+          </div>
+        </div>
+      </div>
+    </div>
+
   </div>
 </template>
 
 <style lang="scss" scoped>
 @use "../scss/partials/_variables.scss" as *;
+
+
+
+.color-text{
+    color:#757575 !important;
+}
+
+button {
+  background-color: #3535ec !important;
+}
+
 
 
 </style>
