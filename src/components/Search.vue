7e922c7829846830784b386edfd4b856c4eb3030
LorenzoPorrega <lorenzoporrega@hotmail.it>
2023-11-06 22:41:06 +0100

    Filter layout

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -19,38 +19,73 @@ export default {
       aria-controls="offcanvasTop">Ricerca..</button>
     <div class="offcanvas offcanvas-top border rounded-5" tabindex="-1" id="offcanvasTop"
       aria-labelledby="offcanvasTopLabel">
-      <div class="offcanvas-header border rounded-top">
-        <h5 class="offcanvas-title" id="offcanvasTopLabel">Offcanvas top</h5>
+      <div class="offcanvas-header border rounded-bottom rounded-5">
+        <div class="w-100 d-flex justify-content-center">
+          <h5 class="offcanvas-title" id="offcanvasTopLabel">Filter</h5>
+        </div>
         <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
       </div>
-      <div class="offcanvas-body border rounded-bottom">
+      <div class="offcanvas-body border rounded-top rounded-5">
         <form action="">
           <div class="container">
             <div class="row d-flex">
+
+              <div class="mb-3 col-12">
+                <label class="form-label fw-bold fs-5">Price range</label>
+                <div id="input">
+
+                  <form class="multi-range-field my-5 pb-5">
+                    <input id="multi22" class="multi-range w-100" type="range" multiple="multiple"/>
+                  </form>
+
+                  <!-- Example -->
+                  <div class="row">
+                    <div class="col-5">
+                      <label for="ex3">Minimum</label>
+                      <input type="text" id="ex3" class="form-control">
+                    </div>
+                    <div class="col-2">
+                      <div class=w-100>
+
+                      </div>
+                    </div>
+                    <div class="col-5">
+                      <label for="ex4">Maximum</label>
+                      <input type="text" id="ex4" class="form-control">
+                    </div>
+                  </div>
+                  
+                </div>
+              </div>
+
               <div class="mb-3 col-6">
-                <label class="form-label fw-bold fs-5">Address</label>
+                <label class="form-label fw-bold fs-5">Location</label>
                 <div id="input">
                   <input type="text" class="form-control" value="" name="address" id="address">
                 </div>
               </div>
+
               <div class="mb-3 col-6">
-                <label class="form-label fw-bold fs-5">Address</label>
+                <label class="form-label fw-bold fs-5">Bedrooms</label>
                 <div id="input">
                   <input type="text" class="form-control" value="" name="address" id="address">
                 </div>
               </div>
+
               <div class="mb-3 col-6">
-                <label class="form-label fw-bold fs-5">Address</label>
+                <label class="form-label fw-bold fs-5">Beds</label>
                 <div id="input">
                   <input type="text" class="form-control" value="" name="address" id="address">
                 </div>
               </div>
+
               <div class="mb-3 col-6">
-                <label class="form-label fw-bold fs-5">Address</label>
+                <label class="form-label fw-bold fs-5">Bathrooms</label>
                 <div id="input">
                   <input type="text" class="form-control" value="" name="address" id="address">
                 </div>
               </div>
+
             </div>
           </div>
         </form>
@@ -71,10 +106,10 @@ export default {
   }
 
   .offcanvas {
-    width: 70vw;
+    width: 44vw;
     height: 50vh;
     top: calc(50% - 25vh);
-    left: calc(50% - 35vw);
+    left: calc(50% - 22vw);
   }
 }
 </style>
