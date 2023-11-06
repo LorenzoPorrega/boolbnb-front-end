f232006153e191b011863d93722b7988272d3bdd
LorenzoPorrega <lorenzoporrega@hotmail.it>
2023-11-06 17:44:47 +0100

    fixed apartmentspage layout

--- src/pages/ApartmentsPage.vue
+++ src/pages/ApartmentsPage.vue
@@ -3,57 +3,45 @@ import axios from 'axios';
 import Card from "../components/Card.vue";
 import { store } from '../store.js';
 
-    export default {
-        data() {
-            return {
-                store,
-            }
-        },
-        components:{
-          Card,
-        },
-        methods: {
-            fetchData() {
-                axios.get("http://127.0.0.1:8000/api/apartments/")
-                .then((response) => {
-                    const results = response.data
-                    console.log(results)
-                })
-            },
-        },
-        mounted(){
-            this.fetchData();
-        }
+export default {
+  data() {
+    return {
+      store,
+    }
+  },
+  components: {
+    Card,
+  },
+  methods: {
+    fetchData() {
+      axios.get("http://127.0.0.1:8000/api/apartments/")
+        .then((response) => {
+          const results = response.data
+          console.log(results)
+        })
+    },
+  },
+  mounted() {
+    this.fetchData();
+  }
 
 }
 </script>
 
 <template>
-    <h1>APPARTAMENTI</h1>
-
-    <div class="container">
+  <div class="container">
     <div class="row-cols-4 d-flex">
-        <div class="col-3" v-for="singleApartment in this.apartments" :key="singleApartment.id">
-            <Card :singleApartment="singleApartment"></Card>
-        </div>
+      <div class="col-3" v-for="singleApartment in this.apartments" :key="singleApartment.id">
+        <Card :singleApartment="singleApartment"></Card>
+      </div>
     </div>
-</div>
-
+  </div>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
 @use "../scss/partials/_variables.scss" as *;
 
-.color-text{
-    color:#757575 !important;
-}
-
-.container{
+.container {
   height: calc(100vh - 300px);
 }
-
-button {
-  background-color: #3535ec !important;
-}
-
 </style>
