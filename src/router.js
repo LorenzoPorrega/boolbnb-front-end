72e9b10630a2ea3a99035141953edf8b7704ffdb
acolombo09 <acolombo0911@gmail.com>
2023-11-07 13:57:42 +0100

    added-page404-component

--- src/router.js
+++ src/router.js
@@ -1,12 +1,19 @@
 import { createRouter, createWebHistory } from "vue-router"; 
 
-import ApartmentsPage from "./pages/ApartmentsPage.vue"
+import ApartmentsPage from "./pages/ApartmentsPage.vue";
+// import componente jolly per url sbagliati o sconosciuti richiesti dall'utente
+import NotFound from "./pages/NotFound.vue";
 
 const routes = [
     {
         path: "/",
         name: "home",
         component: ApartmentsPage
+    },
+    {
+        path: "/:pathMatch(.*)*",
+        name: "not-found",
+        component: NotFound,
     }
 ]
 const router = createRouter({
