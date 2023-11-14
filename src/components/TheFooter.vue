60cab2e59571f404fa1b57067b5fc6e5f17c2930
acolombo09 <acolombo0911@gmail.com>
2023-11-14 16:57:10 +0100

    footer-completed

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -12,126 +12,122 @@ export default {
 
   <footer class="">
     <div class="py-3">
-      <div class="row">
-        <div class="col-md-12 d-flex justify-content-between">
-          <div class="col-md-4 d-flex flex-column justify-content-center align-items-center">
-            <h5 class="footer-heading">Support</h5>
-            <ul class="list-unstyled d-flex flex-column justify-content-center align-items-center">
-              <li>
-                <a href="#">Help Center</a>
-              </li>
-              <li>
-                <a href="#">BoolCover</a>
-              </li>
-              <li>
-                <a href="#">Anti-discrimination</a>
-              </li>
-              <li>
-                <a href="#">Disability Support</a>
-              </li>
-              <li>
-                <a href="#">Cancellation Options</a>
-              </li>
-              <li>
-                <a href="#">Report Neighborhood Concern</a>
-              </li>
-            </ul>
-          </div>
-          <div class="col-md-4 d-flex flex-column justify-content-center align-items-center">
-            <h5 class="footer-heading">Hosting</h5>
-            <ul class="list-unstyled d-flex flex-column justify-content-center align-items-center">
-              <li>
-                <a href="#">Boolbnb Your Home</a>
-              </li>
-              <li>
-                <a href="#">BoolCover for Hosts</a>
-              </li>
-              <li>
-                <a href="#">Hosting Resources</a>
-              </li>
-              <li>
-                <a href="#">Community Forum</a>
-              </li>
-              <li>
-                <a href="#">Hosting Responsibly</a>
-              </li>
-              <li>
-                <a href="#">Boolbnb Friendly Apartments</a>
-              </li>
-            </ul>
-          </div>
-          <div class="col-md-4 d-flex flex-column justify-content-center align-items-center">
-            <h5 class="footer-heading">Boolbnb</h5>
-            <ul class="list-unstyled d-flex flex-column justify-content-center align-items-center">
-              <li>
-                <a href="#">Newsroom</a>
-              </li>
-              <li>
-                <a href="#">New Features</a>
-              </li>
-              <li>
-                <a href="#">Careers</a>
-              </li>
-              <li>
-                <a href="#">Investors</a>
-              </li>
-              <li>
-                <a href="#">Gift Cards</a>
-              </li>
-              <li>
-                <a href="#">Boolbnb.org Emergency Stays</a>
-              </li>
-            </ul>
-          </div>
+      <div class="row justify-content-between">
+        <div class="col-12 col-md-4 d-flex flex-column justify-content-center align-items-center">
+          <h5 class="footer-heading">Support</h5>
+          <ul class="list-unstyled d-flex flex-column justify-content-center align-items-center">
+            <li>
+              <a href="#">Help Center</a>
+            </li>
+            <li>
+              <a href="#">BoolCover</a>
+            </li>
+            <li>
+              <a href="#">Anti-discrimination</a>
+            </li>
+            <li>
+              <a href="#">Disability Support</a>
+            </li>
+            <li>
+              <a href="#">Cancellation Options</a>
+            </li>
+            <li>
+              <a href="#">Report Neighborhood Concern</a>
+            </li>
+          </ul>
+        </div>
+        <div class="col-12 col-md-4 d-flex flex-column justify-content-center align-items-center">
+          <h5 class="footer-heading">Hosting</h5>
+          <ul class="list-unstyled d-flex flex-column justify-content-center align-items-center">
+            <li>
+              <a href="#">Boolbnb Your Home</a>
+            </li>
+            <li>
+              <a href="#">BoolCover for Hosts</a>
+            </li>
+            <li>
+              <a href="#">Hosting Resources</a>
+            </li>
+            <li>
+              <a href="#">Community Forum</a>
+            </li>
+            <li>
+              <a href="#">Hosting Responsibly</a>
+            </li>
+            <li>
+              <a href="#">Boolbnb Friendly Apartments</a>
+            </li>
+          </ul>
+        </div>
+        <div class="col-12 col-md-4 d-flex flex-column justify-content-center align-items-center">
+          <h5 class="footer-heading">Boolbnb</h5>
+          <ul class="list-unstyled d-flex flex-column justify-content-center align-items-center">
+            <li>
+              <a href="#">Newsroom</a>
+            </li>
+            <li>
+              <a href="#">New Features</a>
+            </li>
+            <li>
+              <a href="#">Careers</a>
+            </li>
+            <li>
+              <a href="#">Investors</a>
+            </li>
+            <li>
+              <a href="#">Gift Cards</a>
+            </li>
+            <li>
+              <a href="#">Boolbnb.org Emergency Stays</a>
+            </li>
+          </ul>
         </div>
       </div>
     </div>
 
     <div class="py-3 border-top">
-      <div class="row">
-        <div class="col-md-12 d-flex flex-wrap justify-content-between align-items-center">
-          <div class="col-md-4 mb-0 d-flex flex-nowrap justify-content-center">
-            <span class="me-2">© 2023 Boolbnb, Inc.</span>
-            <a href="#" class="me-2">· Terms</a>
-            <a href="#">· Privacy</a>
-          </div>
+      <div class="row flex-wrap justify-content-between align-items-center g-3">
+        <div class="col-12 col-md-4 mb-0 d-flex flex-nowrap justify-content-center">
+          <span class="me-2">© 2023 Boolbnb, Inc.</span>
+          <a href="#" class="me-2">· Terms</a>
+          <a href="#">· Privacy</a>
+        </div>
 
-          <a href="/" class="col-md-4 d-flex align-items-center justify-content-center">
-            <img src="/images/boolbnb-logo-2.png" alt="footer-logo">
-          </a>
+        <a href="/" class="col-12 col-md-4 d-flex align-items-center justify-content-center">
+          <img src="/images/boolbnb-logo-2.png" alt="footer-logo">
+        </a>
 
-          <div class="col-md-4 d-flex flex-nowrap justify-content-center">
-            <div class="me-2">
-              <a href="#" class="social-icon">
-                <i class="fa-solid fa-globe"></i>
-                <a href="#">
-                  English (US)
-                </a>
-              </a>
-            </div>
-            <div class="me-2">
-              <a href="#" class="social-icon">
-                <i class="fa-solid fa-dollar-sign"></i>
-              </a>
+        <div class="col-12 col-md-4 d-flex flex-nowrap justify-content-center">
+          <div class="me-2">
+            <a href="#" class="social-icon">
+              <i class="fa-solid fa-globe"></i>
               <a href="#">
-                USD
+                English (US)
               </a>
-            </div>
-            <div class="me-2">
-              <a href="#" class="social-icon">
-                <i class="fab fa-facebook-f"></i>
-              </a>
-            </div>
-            <div class="me-2">
-              <a href="#" class="social-icon">
-                <i class="fab fa-twitter"></i>
-              </a>
-            </div>
-            <div>
-              <a href="#" class="social-icon">
-                <i class="fab fa-instagram"></i>
-              </a>
-            </div>
+            </a>
+          </div>
+          <div class="me-2">
+            <a href="#" class="social-icon">
+              <i class="fa-solid fa-dollar-sign"></i>
+            </a>
+            <a href="#">
+              USD
+            </a>
+          </div>
+          <div class="me-2">
+            <a href="#" class="social-icon">
+              <i class="fab fa-facebook-f"></i>
+            </a>
+          </div>
+          <div class="me-2">
+            <a href="#" class="social-icon">
+              <i class="fab fa-twitter"></i>
+            </a>
+          </div>
+          <div>
+            <a href="#" class="social-icon">
+              <i class="fab fa-instagram"></i>
+            </a>
           </div>
         </div>
       </div>
