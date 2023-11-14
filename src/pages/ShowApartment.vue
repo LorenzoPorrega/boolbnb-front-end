a4f8b84c5228e7e6c147c63622ac9a0b1f4ad60b
MicheleGresta <malachia_1997@hotmail.it>
2023-11-14 11:47:17 +0100

    remove

--- src/pages/ShowApartment.vue
+++ src/pages/ShowApartment.vue
@@ -2,6 +2,7 @@
 import axios from 'axios';
 import AmenityCard from "../components/AmenityCard.vue";
 import AmenityCard from "../components/AmenityCard.vue";
+import AmenityCard from "../components/AmenityCard.vue";
 import { store, createmap, getFrontEndCostumerIP } from '../store.js';
 import ShowCarousel from '../components/ShowCarousel.vue';
 
@@ -11,6 +12,8 @@ export default {
       store,
       host: {},
       amenities: [],
+      host: {},
+      amenities: []
       host: {},
       amenities: []
     };
@@ -26,6 +29,8 @@ export default {
         axios.get("http://127.0.0.1:8000/api/selected/" + this.$route.params.slug)
           .then(response => {
             // Saves the response in the local showedApartment object
+            // console.log("Funzione per prendere dati appartamento visionato in show startata, oggetto singleApartmanet:");
+            // console.log(response);
             // console.log("Funzione per prendere dati appartamento visionato in show startata, oggetto singleApartmanet:");
             // console.log(response);
             store.showedApartment = response.data.showedApartment[0];
@@ -49,6 +54,15 @@ export default {
 </script>
 
 <template>
+  <div class="container-fluid py-3 border-bottom" style="margin-top: 150px;">
+    <h2 class="pt-2"><strong>{{ store.showedApartment.title }}</strong></h2>
+    <div class="d-flex">
+      <i class="fa-solid fa-location-dot me-2" style="color: #3737ec;"></i>
+      <h6>{{ store.showedApartment.address }}</h6>
+    </div>
+    <div class="w-30" v-for="showedApartmentImage in store.showedApartment.images">
+      <img class="img-show" :src="`http://127.0.0.1:8000/storage/${showedApartmentImage}`" alt="">
+    </div>
   <div class="container-fluid py-3 border-bottom" style="margin-top: 150px;">
     <h2 class="pt-2"><strong>{{ store.showedApartment.title }}</strong></h2>
     <div class="d-flex">
@@ -109,6 +123,32 @@ export default {
       </div>
     </div>
 
+    <!-- Amenities Section -->
+    <div class="container py-5 border-bottom">
+      <div class="my-4">
+        <h3 class="fw-bold fs-4">What this place offers</h3>
+      </div>
+      <div class="row row-cols-3 row-cols-md-6 g-3">
+        <div class="col" v-for="singleAmenity in store.showedApartment.amenities " :key="singleAmenity.id">
+          <AmenityCard :singleAmenity="singleAmenity"></AmenityCard>
+        </div>
+      </div>
+    </div>
+
+    <div class="mt-5 mb-4">
+      <h3 class="fw-bold fs-4">Where you'll be</h3>
+      <h5>{{ store.showedApartment.address }}</h5>
+    </div>
+    <div class="container-map position-relative mt-3 mb-5">
+      <div class="container">
+        <div class='control-panel'>
+          <div id='store-list'></div>
+        </div>
+        <div class='map' id='map'></div>
+      </div>
+    </div>
+  </div>
+
     <!-- Host Section with contact redirect -->
     <div class="py-5 border-bottom">
       <div class="row py-3">
@@ -142,6 +182,38 @@ export default {
                 <div class="icon">⚠️</div>
                 <p>To protect your payment, never transfer money or communicate outside of the Airbnb website or app.</p>
               </div>
+  <!-- Host Section with contact redirect -->
+  <div class="py-5 border-bottom">
+    <div class="row py-3">
+      <div class="col-3 d-flex justify-content-center align-items-start">
+        <img src="/images/lporrega.JPG" alt="Host-Avatar" class="host-avatar">
+      </div>
+      <div class="col-9 host-info d-flex">
+        <div class="row">
+          <div class="col-7">
+            <h3>Hosted by {{ this.host.name }}</h3>
+            <ul>
+              <li>Joined in May 2023</li>
+              <li><span class="icon"><i class="fa-solid fa-star"></i></span>164 Reviews</li>
+              <li><span class="icon"><i class="fa-solid fa-user-check"></i></span>Identity verified</li>
+              <li><span class="icon"><i class="fa-solid fa-medal"></i></span> Superhost</li>
+            </ul>
+            <p>Hello everyone! I’m Lorenzo. <br>I really enjoy travelling and I work in real estate!</p>
+            <p class="superhost-badge"><strong>{{ this.host.name }}</strong> is a Superhost</p>
+            <p>Superhosts are experienced, highly rated hosts who are committed to providing great stays for guests.</p>
+          </div>
+          <div class="col-5">
+            <ul>
+              <li>Language: Italiano</li>
+              <li>Response rate: 100%</li>
+              <li>Response time: within an hour</li>
+            </ul>
+            <router-link :to="{ name: 'messages' }" class="btn btn-primary contact-host-btn text-decoration-none">
+              Contact Host
+            </router-link>
+            <div class="payment-warning d-flex mt-4">
+              <div class="icon">⚠️</div>
+              <p>To protect your payment, never transfer money or communicate outside of the Airbnb website or app.</p>
             </div>
           </div>
         </div>
@@ -160,6 +232,8 @@ export default {
 }
 
 .container-map {
+  height: 75vh;
+  width: 100%;
   height: 75vh;
   width: 100%;
   height: 75vh;
@@ -205,6 +279,7 @@ export default {
 }
 
 
+/* Stili per la sezione dell'host */
 /* Stili per la sezione dell'host */
 /* Stili per la sezione dell'host */
 
